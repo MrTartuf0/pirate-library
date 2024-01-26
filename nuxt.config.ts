6be86f4dbd7d4189a4d81f521ced7a156b4a1718
Vitaliy Didyk <79630556+MrTartuf0@users.noreply.github.com>
2024-01-26 13:40:41 +0100

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  modules: ['@nuxt/ui'],
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <UContainer>
+    <h1>!!!!</h1>
+  </UContainer>
+</template>
